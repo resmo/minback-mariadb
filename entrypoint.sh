f75beb95e0f59271b99129bf790bcff9f3a492dc
Rene Moser <mail@renemoser.net>
2023-08-13 15:09:10 +0200

    adjust output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e -o pipefail
 
+echo "Start backup run"
+
 mc config host add pg "$MINIO_SERVER" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY" --api "$MINIO_API_VERSION" > /dev/null
 
 ARCHIVE="${MINIO_BUCKET}/${DB}-$(date "$DATE_FORMAT").archive"
@@ -9,4 +11,5 @@ echo "Dumping $DB to $ARCHIVE from $DB_HOST:$DB_PORT with user $DB_USER"
 
 mariadb-dump "$DB" --host="$DB_HOST" --port="$DB_PORT" --user="$DB_USER" --password="$DB_PASSWORD" | mc pipe "pg/$ARCHIVE" || { echo "Backup failed"; mc rm "pg/$ARCHIVE"; exit 1; }
 
+echo ""
 echo "Backup complete"
